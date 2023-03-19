aec769918a70a0ea7e5ad1ad44d206930f0f72b2
Md Momin <md808775@gmail.com>
2023-03-19 17:59:28 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Github a second time kivabe upload korbi dekh, 
+// 1dom simple
+
+// First time
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+})
