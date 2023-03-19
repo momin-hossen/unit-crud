48e3fd62cfc17ab616585af9153654e2877dade7
Md Momin <md808775@gmail.com>
2023-03-19 18:04:02 +0600

    Give some instractions to upload on github.

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,19 @@ use Illuminate\Support\Facades\Route;
 
 // First time
 
+// Porer bar theke kono change paile ey jaygay files er list gulu dekhte parbi.
 
+// Jemon dekh, web.php te change korsi, oi jonnoi ey jaygay list chole ashche,
+
+// ekhn toi oi file a click korle dekhte parbi ki ki change korsos, 
+
+// so, eto kisu dekha tur kaj nah
+// Uporer jei 1ta input field ase(Message sidebar a ), oi jaygay just 1ta message dibi, jekono (Or tui ki kaj korli oita dite paros) ---- Like -> Category list show.
+
+// Ekhno dekh kivabe kori,
+
+// then just commit a click korlei kaj sesh--- 1dom simple.
 
 Route::get('/', function () {
     return view('welcome');
-})
+});
